cfa9418b0db80a348fdf86f0e9618ed2c800ddf1
roykhan99 <roykhananto99@gmail.com>
2022-05-31 14:41:02 +0700

    Initial commit

---
+++ app/src/main/java/bangkit/project/cato/ui/splashScreen/SplashScreen.kt
@@ -0,0 +1,12 @@
+package bangkit.project.cato.ui.splashScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import bangkit.project.cato.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
